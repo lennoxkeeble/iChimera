dfd1c4f79728df728e4fef558089e62af111ee4b
lennoxkeeble <72128853+lennoxkeeble@users.noreply.github.com>
2024-01-08 14:20:27 -0500

    Delete NumericalRelativity directory

--- NumericalRelativity/main.jl
+++
@@ -1,7 +0,0 @@
-include("Kerr.jl")
-include("NumericalGeodesics.jl")
-include("HarmonicCoords.jl")
-include("SelfForce.jl")
-include("KludgeWaveforms.jl")
-include("EffPotential.jl")
-include("GRPlotLib.jl");
