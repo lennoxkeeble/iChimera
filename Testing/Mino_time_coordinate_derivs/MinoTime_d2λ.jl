3c1de2fe6c2abb1f9d93bdc307160ee07d6f5888
lennoxkeeble <72128853+lennoxkeeble@users.noreply.github.com>
2024-09-21 16:32:12 -0400

    Delete Testing directory

--- Testing/Mino_time_coordinate_derivs/MinoTime_d2λ.jl
+++
@@ -1,9 +0,0 @@
-module MinoDerivs2
-d2t_dλ(x::AbstractArray, dx::AbstractArray, a::Float64, M::Float64, E::Float64, L::Float64, C::Float64) = (2*(a^3*(a*E - L)*M + x[1]*(a^4*E + x[1]*(a*(-2*a*E + L)*M + E*x[1]*(2*a^2 - 3*M*x[1] + x[1]^2))))*dx[1])/(a^2 - 2*M*x[1] + x[1]^2)^2 - a^2*E*sin(2*x[2])*dx[2]
-
-d2r_dλ(x::AbstractArray, dx::AbstractArray, a::Float64, M::Float64, E::Float64, L::Float64, C::Float64) = (-((-2*M + 2*x[1])*(C + (-(a*E) + L)^2 + x[1]^2)) - 2*x[1]*(a^2 - 2*M*x[1] + x[1]^2) + 4*E*x[1]*(-(a*L) + E*(a^2 + x[1]^2)))/2.
-
-d2θ_dλ(x::AbstractArray, dx::AbstractArray, a::Float64, M::Float64, E::Float64, L::Float64, C::Float64) = (2*L^2*cot(x[2])*csc(x[2])^2 + 2*a^2*(1 - E^2)*cos(x[2])*sin(x[2]))/2.
-
-d2ϕ_dλ(x::AbstractArray, dx::AbstractArray, a::Float64, M::Float64, E::Float64, L::Float64, C::Float64) = (2*a*(a*(a*E - L)*M + a*L*x[1] - E*M*x[1]^2)*dx[1])/(a^2 - 2*M*x[1] + x[1]^2)^2 - 2*L*cot(x[2])*csc(x[2])^2*dx[2]
-end
--- Testing/Prop_time_coordinate_derivs/derivs.jl
+++
@@ -1,8 +0,0 @@
-using StaticArrays
-include("d2x.jl")
-include("d3x.jl")
-include("d4x.jl")
-include("d5x.jl")
-# include("d6x.jl")
-# include("d7x.jl")
-# include("d8x.jl")
